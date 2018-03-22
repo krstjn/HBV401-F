b3b34b65e9d9af7fada5a91e27c9a666def0b1c5
jonthorsteinsson <jth56@hi.is>
2018-03-22 12:36:35 +0000

    Flight class - Bætt við price DBmanager - searchFlights() og getFlights().

--- src/is/hi/controller/DBManager.java
+++ src/is/hi/controller/DBManager.java
@@ -1,5 +1,6 @@
 package is.hi.controller;
 
+import is.hi.model.Flight;
 import is.hi.model.Query;
 
 import java.sql.*;
@@ -10,7 +11,12 @@ import java.util.ArrayList;
  * Höfundur: Kristján P.*
  ************************/
 public class DBManager {
+
     private Connection conn;
+
+    private ArrayList<Flight> flights;
+    private ArrayList<Query> searches;
+
     public DBManager(){
         try{
             System.out.println("Trying sqlite...");
@@ -33,4 +39,34 @@ public class DBManager {
         System.out.println();
         return a;
     }
+
+    public void searchFlights(String query) throws SQLException {
+        PreparedStatement p = conn.prepareStatement(query);
+        ResultSet r = p.executeQuery();
+
+        Flight flight = new Flight();
+        while (r.next()) {
+            flight.setFlightID(r.getString("flightID"));
+            flight.setFrom(r.getString("origin"));
+            flight.setTo(r.getString("destination"));
+            flight.setDepartureTime(r.getString("departure"));
+            flight.setArrivalTime(r.getString("arrival"));
+            flight.setEcoCapacity(r.getInt("ecoCapacity"));
+            flight.setBusCapacity(r.getInt("busCapacity"));
+            flight.setEcoPrice(r.getInt("ecoPrice"));
+            flight.setBusPrice(r.getInt("busPrice"));
+            flight.setAirline(r.getString("airline"));
+
+            flights.add(flight);
+        }
+        r.close();
+        conn.close();
+        System.out.println();
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+
 }
